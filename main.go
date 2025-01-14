841ad8eb91808ac32e0aba048161a5a1c727965a
ogarridojimenez <ogarridojimenez@gmail.com>
2025-01-13 23:49:17 -0500

    three commit

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+type Repository struct {
+	DB *gorm.DB
+}
+
+func (r *Repository) SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/create_books", r.CreateBook)
+	api.Delete("delete_book/:id", r.DeleteBook)
+	api.Get("/get_books/:id", r.GetBookbyID)
+	api.Get("books", r.GetBooks)
+}
+
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := Repository{
+		DB: db,
+	}
+	app := fiber.New()
+	r.SetupRoutes(app)
+	app.Listen(":8080")
 
 }
